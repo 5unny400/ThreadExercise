83c5be208b451b3c64a626984e8a17370775240a
5unny400 <3407601546@qq.com>
2020-10-08 21:23:46 +0800

    duo xian cheng

--- src/exercise01/exercise01.java
+++ src/exercise01/exercise01.java
@@ -1,36 +1,44 @@
 package exercise01;
 
+/**
+ * @Author Shenxinyuan
+ * @Description //TODO
+ * @Date 19:53 2020/10/8 0008
+ * @Param
+ * @return
+ **/
 public class exercise01 {
     public static void main(String[] args) {
 
-        Runnable runable = new MyRunable();
-
+        Thread thread01 = new MyThread("A");
+        Thread thread02 = new MyThread("B");
+        Thread thread03 = new MyThread("C");
         try {
-//            Thread.sleep(3000);//主线程睡眠，后边的线程也不能继续
-            System.out.println(Thread.currentThread().getName()+" 主线程开始");
-
-            Thread thread01 = new Thread(runable, "A");
-            Thread thread02 = new Thread(runable, "B");
-            Thread thread03 = new Thread(runable, "C");
-
-            thread01.join(3000);
             thread01.start();
-            thread02.join(2000);
+            thread01.join();
+
             thread02.start();
-            thread03.join(1000);
+            thread02.join();
+
+            thread03.join();
             thread03.start();
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        }finally{
-            System.out.println(Thread.currentThread().getName()+" 主线程结束");
         }
+
 
     }
 }
 
-class MyRunable implements Runnable{
+class MyThread extends Thread {
+    String name;
+
+    MyThread(String name) {
+        this.name = name;
+    }
+
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName()+"  Runable接口实现的线程");
+        System.out.print(name);
     }
 }
